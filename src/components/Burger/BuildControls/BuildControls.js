d6e23e8d0abff410a11cf4a52ff1a976103c503a
Vivek <vivekdmistry1999@gmail.com>
2021-02-09 16:16:51 +0530

    Some changes in BurgerControls.js

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,7 +11,7 @@ const controls = [
 
 const BuildControls = (props) => (
     <div className={classes.BuildControls}>
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        <p><em>Current Price:</em>  <strong>$ {props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
